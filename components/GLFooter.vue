a32308e420c146363f28d5290b3b38eb090955cb
MengNianxiaoyao <2589141604@qq.com>
2025-08-30 11:48:41 +0800

    更新

--- components/GLFooter.vue
+++ components/GLFooter.vue
@@ -5,12 +5,14 @@ import GLFooter from 'valaxy-theme-gitlink/components/GLFooter.vue'
 <template>
   <GLFooter>
     <div class="footer-powered" m="y-2">
-        <span style="font-weight: bold;font-style: italic;">Powered by</span>
-        <div class="footer-logo" style="gap: 20px;">
-            <a href="https://vercel.com" i-logos-vercel></a>
-            <a href="https://cloudflare.com" i-logos-cloudflare></a>
-            <a href="https://edgeone.ai"><img src="https://edgeone.ai/media/34fe3a45-492d-4ea4-ae5d-ea1087ca7b4b.png" width="150px"></a>
-        </div>
+      <span style="font-weight: bold;font-style: italic;">Powered by</span>
+      <div class="footer-logo" style="gap: 20px;">
+        <a href="https://vercel.com" i-logos-vercel></a>
+        <a href="https://cloudflare.com" i-logos-cloudflare></a>
+        <a href="https://console.cloud.tencent.com/edgeone">
+          <img src="https://edgeone.ai/media/34fe3a45-492d-4ea4-ae5d-ea1087ca7b4b.png" width="150px">
+        </a>
+      </div>
     </div>
   </GLFooter>
 </template>
